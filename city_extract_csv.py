7e89b80752525544effa9d4d6c006b922f0e20c8
Christine Phan <cxphan@stanford.edu>
2019-02-11 16:27:24 -0500

    Added new CSV version

--- city_extract_csv.py
+++ city_extract_csv.py
@@ -2,6 +2,15 @@ import os
 import sys
 import re 
 
+# functions for debugging
+# Disable
+def blockPrint():
+    sys.stdout = open(os.devnull, 'w')
+
+# Restore
+def enablePrint():
+    sys.stdout = sys.__stdout__
+
 def toCSV(city, state, category, tier, city_stat, employment_stat, industry_stat):
 	headers = ['city', 'state', 'category', 'tier'] + city_stat.keys() + employment_stat.keys() + industry_stat.keys()
 	values = [city, state, category, tier] + city_stat.values() + employment_stat.values() + industry_stat.values()
@@ -11,8 +20,8 @@ def writeDictionary(dictionary, data):
 	dict2 = {}
 	for key in dictionary.keys(): 
 		pattern = dictionary[key]
-		# print pattern
 		temp = re.findall(pattern, data)
+		# print pattern
 		# print temp
 
 		if len(temp) > 0:
@@ -23,8 +32,8 @@ def writeDictionary(dictionary, data):
 				dict2[key] = temp[0].encode('utf-8')
 		else:
 			dict2[key] = 'N/A'
-	print dict2
 
+	# print dict2
 	return dict2
 
 def infras(data):
@@ -39,6 +48,11 @@ def infras(data):
 	infrastructure['sewerage'] = "Type of sewerage system\*,+\"([\w\n ]+)\""
 	infrastructure['type'] = "Type of solid Waste system\*,+([\w\n ]+)"
 	infrastructure['mobile-ownership'] = "% of households with access to mobile phones,+([0-9.]+)"
+	infrastructure['internet-computer'] = "\"?% of households with access to computer\/laptop\nwith internet\n?without internet\",+\"?([0-9.]+)\n[0-9.]+"
+	infrastructure['no-internet-computer'] = "\"?% of households with access to computer\/laptop\nwith internet\n?without internet\",+\"?[0-9.]+\n([0-9.]+)"
+	infrastructure['housing-owned'] = "Owned\nRented\",+\"([0-9.]+)\n[0-9.]+"
+	infrastructure['housing-rented'] = "Owned\nRented\",+\"[0-9.]+\n([0-9.]+)"
+	
 	infrastructure_stat = writeDictionary(infrastructure, data)
 	return infrastructure_stat
 
@@ -50,40 +64,23 @@ def industry(data):
 	industry['professionals'] = "\"?Professionals\"?,+([0-9.]+)"
 	industry['technicians'] = "\"?Technicians and associate professionals\"?,+([0-9.]+)"
 	industry['clerks'] = "\"?Clerks\"?,+([0-9.]+)"
-	industry['service'] = "\"?Service workers and shop and market sales ?\n?workers\"?,([0-9.]+)"
+	industry['service'] = "\"?Service workers and shop and market sales ?\n?workers\"?,+([0-9.]+)"
 	industry['agriculture'] = "\"?Skilled agricultural and fishery workers\"?,+([0-9.]+)"
 	industry['craft'] = "\"?Craft and related trades workers\"?,+([0-9.]+)"
 	industry['machine-operators'] = "\"?Plant and machine operators and\n? ?assemblers\"?,+([0-9.]+)"
-
-	for key in industry.keys(): 
-		pattern = industry[key]
-
-		temp = re.findall(pattern, data)
-
-		if len(temp) > 0:
-			industry_stat[key] = temp[0].encode('utf-8')
-		else:
-			industry_stat[key] = 'N/A'
-
+	industry_stat = writeDictionary(industry, data)
 	return industry_stat
 
 # employment statistics 
 def employmentstats(data):
+	# print "==== EMPLOYMENT===="
 	employment = {}
 
 	employment['per_capita'] = "Per Capita Income \(Rs.?\) at 2004-05 \D*([0-9]+)"
 	employment['unemployment'] = "\"Unemployment Rate, 2011-12\*\*\*\",([0-9.]+)"
 	employment['working'] = "\"Work Participation Rate, 2011-12\*\*\*\",([0-9.]+)"
-	employment['workstatus'] = "Work Status, 2011-12 \(%\) ([0-9.]+)"
 
-	emp_stat = {}
-	for key in employment.keys():
-		pattern = employment[key]
-		temp = re.findall(pattern, data)
-		if len(temp) > 0:
-			emp_stat[key] = temp[0].encode('utf-8')
-		else:
-			emp_stat[key] = 'N/A'
+	emp_stat = writeDictionary(employment, data)
 
 	return emp_stat
 
@@ -96,7 +93,7 @@ def citystats(data):
 
 	city_text['population'] = "Total Population,\"?([0-9]+)\"?"
 	city_text['urban_population'] = "Total Population of UA \(if\)\,\"?([0-9]+)\"?"
-	city_text['population_growth'] = "Population Growth Rate \(AEGR\) 2001-11,([0-9.]+)"
+	city_text['population_growth'] = "Population Growth Rate \(AEGR\) 2001-11,([-0-9.]+)"
 	city_text['area'] = "Area \(sq\. km\)\*,([0-9.]+)"
 	city_text['density'] = "Density of population \(person per sq\. km\)\*,([0-9]+)"
 	
@@ -106,42 +103,33 @@ def citystats(data):
 	city_text['tribes'] = "Schedule Tribes \(%\),([0-9\.]+)"
 	city_text['youth'] = "Youth, ?15 ?- ?24 ?years ?\(%\)\",([0-9\.]+)"
 	city_text['slum'] = "Slum Population \(%\),([0-9\.]+)"
-	city_text['working'] = "Working Age Group, 15-59 years \(%\),([0-9\.]+)"
+	city_text['working'] = "\"?Working Age Group, 15-59 years \(%\)\"?,([0-9\.]+)"
 
-	for key in city_text.keys():
-		pattern = city_text[key]
-		temp = re.findall(pattern, data)
-		if len(temp) > 0:
-			city_stat[key] = temp[0].encode('utf-8')
-			# print key, city_stat[key]
-		else:
-			city_stat[key] = 'N/A'
+	city_stat = writeDictionary(city_text, data)
 
 	return city_stat
 
 	
 def cities(data):
-	# print "==== CITY STATISTICS ===== "
 	city = re.findall('City: (\w+)', data)[0].encode('utf-8')
 	state = re.findall('State: (\w+ ?\w+)', data)[0].encode('utf-8')
 	category_tier_search = "Category: ([.\s\S]+?), Tier ([0-9])"
 	category, tier = re.findall(category_tier_search, data)[0]
-
 	return city, state, category.encode('utf-8'), tier.encode('utf-8')
 
 def main(): 
-	# print "==== NEW CITY ======= "
+
 	filename = sys.argv[1]
 
 	with open(filename, "r") as myfile:
   		data = myfile.read().decode('utf-8')
 
   	city, state, category, tier = cities(data)
-  	# city_stat = citystats(data)
-  	# employment_stat = employmentstats(data)
-  	# industry_stat = industry(data)
+  	city_stat = citystats(data)
+  	employment_stat = employmentstats(data)
+  	industry_stat = industry(data)
   	infrastructure_stat = infras(data)
-  	# toCSV(city, state, category, tier, city_stat, employment_stat, industry_stat)
+  	toCSV(city, state, category, tier, city_stat, employment_stat, industry_stat)
 	
 if __name__ == "__main__":
     main()
