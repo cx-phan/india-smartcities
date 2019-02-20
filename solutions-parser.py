8be7e33bbd7c56775cda8e66cc0f269e976cca3f
Christine Phan <cxphan@stanford.edu>
2019-02-20 15:34:55 -0500

    Save all for solutions extraction project

--- solutions-parser.py
+++ solutions-parser.py
@@ -3,29 +3,49 @@ import sys
 import re 
 import csv
 
-def write_to_file(city_dictionary):
+# write the same information, but in binary form for easy grouping in Excel/Google Sheets
+def write_to_file_two(city_dictionary, titles):
+	newfile = "solutions-binary.tsv"
+
+ 	# return
+	with open(newfile, 'w') as csvfile:
+	    spamwriter = csv.writer(csvfile, delimiter='\t',
+	                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+	    spamwriter.writerow(["Name"] + titles)
+
+	    for value in city_dictionary.keys():
+	    	has_event = []
+	    	for title in titles:
+	    		has_event.append(title in city_dictionary[value])
+
+	    	spamwriter.writerow([value] + has_event)
+
+# write information in: name of city: projects format 
+def write_to_file_one(city_dictionary):
 	# do something here 
-	newfile = "testfile.csv"
+	newfile = "solutions-group.tsv"
 
 	with open(newfile, 'w') as csvfile:
-	    spamwriter = csv.writer(csvfile, delimiter=',',
+	    spamwriter = csv.writer(csvfile, delimiter='\t',
 	                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
 	    for value in city_dictionary.keys():
-	    	spamwriter.writerow([value] + city_dictionary[value])
+	    	reply = ",".join(city_dictionary[value])
+	    	spamwriter.writerow([value] + [reply])
 
 
 
 def main(): 
-	filename = "solutions.csv"
+	filename = "solutions-original-CSV.csv"
 	city_dictionary = {} 
+	all_jobs = set()
 	with open(filename, 'rb') as csvfile:
 		spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
 		for i, row in enumerate(spamreader):
 			if i == 0:
 				continue
-			# print len(row)
 			all_cities = row[2:len(row)-2]
 			activity = row[1]
+
 			for city in all_cities:
 				name = city.strip("\" ")
 				if name in city_dictionary.keys():
@@ -34,14 +54,10 @@ def main():
 					continue
 				else: 
 					city_dictionary[name] = [activity]
-				# print name
+				all_jobs.add(activity.strip("\""))
 	
-	for value in city_dictionary.keys():
-		print value, city_dictionary[value]
-		print "\n"
-
-	print sorted(city_dictionary.keys())
-	write_to_file(city_dictionary)
+	write_to_file_one(city_dictionary)
+	write_to_file_two(city_dictionary, list(all_jobs))
 
 
 if __name__ == "__main__":
