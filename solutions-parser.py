c852ed30dc2b8f7e8b8a16215f2cd07cfe4fdbbb
Christine Phan <cxphan@stanford.edu>
2019-02-20 01:28:49 -0500

    Added processing

--- solutions-parser.py
+++ solutions-parser.py
@@ -3,16 +3,45 @@ import sys
 import re 
 import csv
 
+def write_to_file(city_dictionary):
+	# do something here 
+	newfile = "testfile.csv"
+
+	with open(newfile, 'w') as csvfile:
+	    spamwriter = csv.writer(csvfile, delimiter=',',
+	                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+	    for value in city_dictionary.keys():
+	    	spamwriter.writerow([value] + city_dictionary[value])
+
+
 
 def main(): 
 	filename = "solutions.csv"
+	city_dictionary = {} 
 	with open(filename, 'rb') as csvfile:
 		spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-		for row in spamreader:
-			print len(row)
-			print row[2:len(row)-2]
-			# print "What is this?" + row[len(row)-1]
-			print row[1]
+		for i, row in enumerate(spamreader):
+			if i == 0:
+				continue
+			# print len(row)
+			all_cities = row[2:len(row)-2]
+			activity = row[1]
+			for city in all_cities:
+				name = city.strip("\" ")
+				if name in city_dictionary.keys():
+					city_dictionary[name] += [activity]
+				elif name == "":
+					continue
+				else: 
+					city_dictionary[name] = [activity]
+				# print name
+	
+	for value in city_dictionary.keys():
+		print value, city_dictionary[value]
+		print "\n"
+
+	print sorted(city_dictionary.keys())
+	write_to_file(city_dictionary)
 
 
 if __name__ == "__main__":
